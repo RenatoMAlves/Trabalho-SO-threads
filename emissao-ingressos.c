2ce2af8720c9155d7fab6c76f38cbb1b13ea11ff
Renato Marinho Alves <renato.mar.alves@gmail.com>
2018-10-16 20:26:36 -0300

    Correções no programa

--- emissao-ingressos.c
+++ emissao-ingressos.c
@@ -1,4 +1,4 @@
-﻿#include <stdio.h>
+#include <stdio.h>
 #include <pthread.h>
 #include <stdbool.h>
 
@@ -26,24 +26,23 @@ void *comprador(void *comp){
 	bool flag = false;
 	for(;;){
 		for(int i=0;i<30;i++){
-			printf("\n%s", ingressos[i]);
 			pthread_mutex_lock(&ExcMutua);
 			if(ingressos[i]=='e'){
-				puts("\nteste");
-				sprintf(ingressos[i], "%c", numero_comprador);
-				printf("\n Comprador: %c - Poltrona: %d", numero_comprador, i);
+				ingressos[i] = numero_comprador;
+				printf("\n Comprou --> Comprador: %c - Poltrona: %d\n", numero_comprador, i);
 				flag = true;
 			}
 			pthread_mutex_unlock(&ExcMutua);
 		}
 		if(flag != true){
-			printf("\n%s Ainda não foram emitidos novos ingressos, por favor aguarde", ((struct Comprad*)comp)->nome);
+			printf("\n%s Ainda não foram emitidos novos ingressos, por favor aguarde\n", ((struct Comprad*)comp)->nome);
 		}
+		if(ingressos[29] != 'n' && ingressos[29] != 'e'){
+			puts("teste");
+			pthread_exit(0);
+		}
+		flag = false;
 		sleep(1);
-	}
-	if(strcmp(ingressos[29],'n') != 0 && strcmp(ingressos[29],'e') != 0){
-		puts("teste");
-		pthread_exit(0);
 	}
 }
 
@@ -63,12 +62,12 @@ int main(){
 	for(int i=0;i<30;i++){
 		ingressos[i] = 'n';
 	}
-
-
+	
+	
 	pthread_create(&emissor, 0, emitir, "");
 	pthread_create(&comprador1, 0, comprador, (void *)comp1);
 	pthread_create(&comprador2, 0, comprador, (void *)comp2);
-
+	
 	pthread_join(emissor, 0);
 	pthread_join(comprador1, 0);
 	pthread_join(comprador2, 0);
@@ -76,6 +75,6 @@ int main(){
 	printf("\n Os ingressos estão esgotados");
 	puts("Relação Poltrona / Comprador");
 	for(int i=0;i<30;i++){
-		printf("\n Poltrona: %d - Comprador: %d", i, ingressos[i]);	
+		printf("Poltrona: %d - Comprador: %c\n", i, ingressos[i]);	
 	}
 }
